1a8363febc2fd68eb39db41516131a3c162a1b61
bbekjava <bibek.gupta@okstate.edu>
2018-12-12 18:11:45 -0600

    More

--- BoomerSooner.java
+++ BoomerSooner.java
@@ -1,18 +1,18 @@
 public class BoomerSooner {
     	public static void main(String args[]) {
 		System.out.println("\nDivided by 3 print Boomer: ");		
-		for (int i=1; i<100; i++) {
+		for (int i=1; i<1000; i++) {
 			if (i%3==0) 
 			System.out.print(i +", ");			
 		}			
 				
 		System.out.println("\n\nDivided by 5 print Sooner: ");
-		for (int i=1; i<100; i++) {
+		for (int i=1; i<1000; i++) {
 			if (i%5==0) System.out.print(i +", ");			
 		}
 				
 		System.out.println("\n\nDivided by 3 & 5 print Boomer Sooner: ");			
-		for (int i=1; i<100; i++) {
+		for (int i=1; i<1000; i++) {
 			if (i%3==0 && i%5==0) System.out.print(i +", ");			
 		}
 		System.out.println("\n");
